3db36374db0d319fa263e11911a5063824b003c2
Travis Kohlbeck <me+git@travisk.info>
2018-01-18 13:59:09 -0800

    checks db for tag/column, checks for tag values

--- datum.py
+++ datum.py
@@ -18,24 +18,35 @@ def main():
 @click.argument('datum', nargs=-1)
 def add(datum):
     '''Add a new datum'''
+    # make sure each tag has a value
+    if len(datum)%2:
+        click.echo('each tag requires a value!')
+        return
+    # TODO parse datum tuple
+
+
     try:
-        with connection.cursor() as cursor:
+        with connection.cursor() as c:
+            sql = 'show columns from datums like %s'
+            # check if column exists, create if not
+            if not c.execute(sql, datum[0]):
+                click.echo('tag not found...')
+                sql = 'alter table datums add %s varchar(32)'
+
             sql = 'insert into datums (time) values (%s)'
-            cursor.execute(sql, datetime.now())
+            c.execute(sql, datetime.now())
         connection.commit()
     finally:
         connection.close()
-
-    click.echo(datum)
 
 @main.command()
 def ls():
     '''List all datums'''
     try:
-        with connection.cursor() as cursor:
+        with connection.cursor() as c:
             sql = 'select * from datums'
-            cursor.execute(sql)
-            click.echo(pprint(cursor.fetchall()))
+            c.execute(sql)
+            click.echo(pprint(c.fetchall()))
     finally:
         connection.close()
 
