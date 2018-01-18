ab6d38bb268f05eadd5ca3afa9c7d6dae2ab8ebf
Travis Kohlbeck <me+git@travisk.info>
2018-01-18 11:26:56 -0800

    add and ls commands working

--- datum.py
+++ datum.py
@@ -1,6 +1,7 @@
 import click
 import pymysql.cursors
-import datetime
+from datetime import datetime
+from pprint import pprint
 
 connection = pymysql.connect(
     host='localhost',
@@ -9,34 +10,35 @@ connection = pymysql.connect(
     cursorclass=pymysql.cursors.DictCursor
 )
 
-try:
-    with connection.cursor() as cursor:
-        sql = 'insert into datums (time) values (%s)'
-        cursor.execute(sql, '2018-01-18 11:11:44')
-    connection.commit()
-finally:
-    connection.close()
-
 @click.group()
 def main():
     pass
 
 @main.command()
-@click.argument('command')
-def test(command):
-    '''A personal data management platform--for humans!'''
-    click.echo(command)
-
-@main.command()
 @click.argument('datum', nargs=-1)
 def add(datum):
     '''Add a new datum'''
+    try:
+        with connection.cursor() as cursor:
+            sql = 'insert into datums (time) values (%s)'
+            cursor.execute(sql, datetime.now())
+        connection.commit()
+    finally:
+        connection.close()
+
     click.echo(datum)
 
 @main.command()
 def ls():
     '''List all datums'''
-    pass
+    try:
+        with connection.cursor() as cursor:
+            sql = 'select * from datums'
+            cursor.execute(sql)
+            click.echo(pprint(cursor.fetchall()))
+    finally:
+        connection.close()
+
 
 @main.command()
 def edit():
