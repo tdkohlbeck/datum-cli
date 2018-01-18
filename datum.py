659dac269742321e4c7266e09aaa86f8681b8944
Travis Kohlbeck <me+git@travisk.info>
2018-01-18 14:53:11 -0800

    able to add new tags/columns, hacky

--- datum.py
+++ datum.py
@@ -7,6 +7,7 @@ connection = pymysql.connect(
     host='localhost',
     user='root',
     db='datum',
+    charset='utf8mb4',
     cursorclass=pymysql.cursors.DictCursor
 )
 
@@ -19,22 +20,20 @@ def main():
 def add(datum):
     '''Add a new datum'''
     # make sure each tag has a value
-    if len(datum)%2:
+    if len(datum) % 2:
         click.echo('each tag requires a value!')
         return
     # TODO parse datum tuple
 
-
     try:
         with connection.cursor() as c:
-            sql = 'show columns from datums like %s'
             # check if column exists, create if not
+            sql = 'show columns from datums like %s'
             if not c.execute(sql, datum[0]):
-                click.echo('tag not found...')
-                sql = 'alter table datums add %s varchar(32)'
-
-            sql = 'insert into datums (time) values (%s)'
-            c.execute(sql, datetime.now())
+                sql = 'alter table datums add ' + datum[0] + ' varchar(32)'
+                c.execute(sql)
+            sql = 'insert into datums (' + datum[0] + ', time) values (%s, %s)'
+            c.execute(sql, (datum[1], datetime.now()))
         connection.commit()
     finally:
         connection.close()
